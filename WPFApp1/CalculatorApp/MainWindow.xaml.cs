d3790769d1d1e9a0b6b70c39691bf0b393133b29
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-12-03 16:16:39 +0530

    Completed Calculato functionality implemented some new c# functionality like Enums and switch case to handle multiple operations also created simpleMth class where we created some methods which contains all functionality of operators

--- WPFApp1/CalculatorApp/MainWindow.xaml.cs
+++ WPFApp1/CalculatorApp/MainWindow.xaml.cs
@@ -20,23 +20,156 @@ namespace CalculatorApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
+        private SelectedOperator _selectedOperator;
         public MainWindow()
         {
             InitializeComponent();
+            AcButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            percentageButton.Click += PercentageButton_Click;
+            equalsButton.Click += EqualsButton_Click;
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (_selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Substraction:
+                        result = SimpleMath.Subtract(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Division(lastNumber, newNumber);
+                        break;
+                }
+
+                resultLabel.Content = result.ToString();
+            }
+        }
+
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber;
+            }
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber;
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
             resultLabel.Content = "0";
         }
 
-        private void SeventhButton_OnClick(object sender, RoutedEventArgs e)
+        private void OperationButton_click(object sender, RoutedEventArgs e)
         {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                resultLabel.Content = "0";
+            }
+
+            if (sender == astrixButton)
+                _selectedOperator = SelectedOperator.Multiplication;
+            if (sender == divisonButton)
+                _selectedOperator = SelectedOperator.Division;
+            if (sender == addButton)
+                _selectedOperator = SelectedOperator.Addition;
+            if (sender == minusButton)
+                _selectedOperator = SelectedOperator.Substraction;
+        }
+        private void NumberButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = 0;
+
+            if (sender == zeroButton)
+                selectedValue = 0;
+            if (sender == firstButton)
+                selectedValue = 1;
+            if (sender == secndButton)
+                selectedValue = 2;
+            if (sender == thirdButton)
+                selectedValue = 3;
+            if (sender == fourthButton)
+                selectedValue = 4;
+            if (sender == fifthButton)
+                selectedValue = 5;
+            if (sender == sixthButton)
+                selectedValue = 6;
+            if (sender == seventhButton)
+                selectedValue = 7;
+            if (sender == eigththButton)
+                selectedValue = 8;
+            if (sender == ninethButton)
+                selectedValue = 9;
+            if (sender == zeroButton)
+                selectedValue = 0;
             if (resultLabel.Content.ToString() == "0")
             {
-                resultLabel.Content = "7";
+                resultLabel.Content = $"{selectedValue}";
             }
             else
             {
-                resultLabel.Content = $"{resultLabel.Content}7";
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
             }
-           
+
+        }
+
+        private void PeriodButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Contains("."))
+            {
+
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}."; 
+            }
+        }
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Add(double n1,double n2)
+        {
+            return n1 + n2;
+        }
+        public static double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public static double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public static double Division(double n1, double n2)
+        {
+            return n1 / n2;
         }
     }
 }
